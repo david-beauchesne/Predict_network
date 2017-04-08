00bbf7fbb8e32154afcfbfdc1f165fa39eff38ed
David Beauchesne <david.beauchesne@uqar.ca>
2017-04-08 12:50:47 -0400

    adjust

--- init.r
+++ init.r
@@ -2,7 +2,7 @@
 rm(list=ls())
  # for use in R console.
  # set own relevant directory if working in R console, otherwise ignore if in terminal
-setwd("/Users/davidbeauchesne/Dropbox/PhD/PhD_obj2/Structure_Comm_EGSL/EGSL_species_distribution")
+setwd("/Users/davidbeauchesne/Dropbox/PhD/PhD_obj2/Structure_Comm_EGSL/Predict_network")
 # -----------------------------------------------------------------------------
 # PROJECT:
 #    Evaluating the structure of the communities of the estuary
